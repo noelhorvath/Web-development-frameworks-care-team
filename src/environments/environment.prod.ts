ad918abe9b6c8c95262f7e3ada5b3dcea635650a
noelhorvath <hnoel2000@gmail.com>
2021-05-09 17:51:27 +0200

    Firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA7AbJ83MlePizfKvTNl7BCC25jaLe0Z6E",
+    authDomain: "care-team-fa069.firebaseapp.com",
+    projectId: "care-team-fa069",
+    storageBucket: "care-team-fa069.appspot.com",
+    messagingSenderId: "468456421576",
+    appId: "1:468456421576:web:96cb4ba17f2c1015e04dfd"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA7AbJ83MlePizfKvTNl7BCC25jaLe0Z6E",
+    authDomain: "care-team-fa069.firebaseapp.com",
+    projectId: "care-team-fa069",
+    storageBucket: "care-team-fa069.appspot.com",
+    messagingSenderId: "468456421576",
+    appId: "1:468456421576:web:96cb4ba17f2c1015e04dfd"
+  }
 };
 
 /*
