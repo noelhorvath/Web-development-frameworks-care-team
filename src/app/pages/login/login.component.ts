90834183ec4f55fd631d1655f461c04009affdb7
noelhorvath <hnoel2000@gmail.com>
2021-05-11 17:47:08 +0200

    Add login and create login layout

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,40 @@ import { Router } from "@angular/router";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.email,Validators.required]),
+    password: new FormControl('',[Validators.required])
+  })
+  alertMessage: any;
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  login(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    this.authService.login(this.form.value.email, this.form.value.password).then(
+      result => {
+        // navigate to home
+      },
+      error => {
+        if (error.code === 'auth/user-not-found') {
+          this.alertMessage = "This email is not registered!"
+        } else if(error.code === 'auth/wrong-password'){
+          this.alertMessage = "Wrong password!"
+        } else{
+          this.alertMessage = "Service is unavailable at the moment!"
+        }
+      }
+    )
+
+
+  }
+
+  navTo(url: string): void {
+    this.router.navigateByUrl(url);
+  }
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent} from "./login.component";
 import { LoginRoutingModule } from "./login-routing.module";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule, LoginRoutingModule
+    CommonModule,
+    LoginRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   exports: [LoginComponent]
 })
