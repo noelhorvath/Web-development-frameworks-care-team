0bce1d1150df057c69e85c64814fee22928deb70
noelhorvath <hnoel2000@gmail.com>
2021-05-16 18:14:55 +0200

    add back id to CareTeam

--- src/app/shared/models/careteam.model.ts
+++ src/app/shared/models/careteam.model.ts
@@ -2,6 +2,7 @@ import {Participant} from "./participant.model";
 import {Period} from "./period.model";
 
 export interface CareTeam{
+  id?: string;
   status: string;
   category?: string[];
   name: string;
