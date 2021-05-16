0fed09292238ad675637dd5a5e502c90dfb200d8
noelhorvath <hnoel2000@gmail.com>
2021-05-16 18:00:32 +0200

    add create method for adding CareTeam to Firestore

--- src/app/services/fb-base.service.ts
+++ src/app/services/fb-base.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, CollectionReference, Query } from "@angular/fire/firestore";
 
 @Injectable({
   providedIn: 'root'
 })
-export class FbBaseService {
+export class FbBaseService<T extends { id?: string }> {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  async create(collectionName: string, data: T, id?: string): Promise<string> {
+    const uid = id ? id : this.afs.createId();
+    data.id = this.afs.createId();
+    await  this.afs.collection(collectionName).doc(uid).set(data);
+    return uid;
+  }
 }
