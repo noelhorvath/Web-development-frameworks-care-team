3c6674ca1b26482ac9316269515cfb6b77884947
noelhorvath <hnoel2000@gmail.com>
2021-05-16 22:58:38 +0200

    configure home-routing.module.ts

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -3,10 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./home.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: 'care-teams', pathMatch: 'full' },
   {
     path: '',
     component: HomeComponent,
-    data: { title: 'Home' }
+    data: { title: 'Home' },
+    children: [
+      {
+        path: 'care-teams',
+        loadChildren: () => import('./../care-team/list/care-team-list.module').then(m => m.CareTeamListModule)
+      },
+      {
+        path: 'view-care-team',
+        loadChildren: () => import('./../care-team/view/care-team-view.module').then(m => m.CareTeamViewModule)
+      }
+    ]
   }
 ];
 
