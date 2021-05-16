4e6d02162752df472e80b86f6ece79ab505716be
noelhorvath <hnoel2000@gmail.com>
2021-05-16 18:36:39 +0200

    generate pages for adding, updating and listing CareTeam(s)

---
+++ src/app/care-team/update/update.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UpdateComponent } from './update.component';
+
+
+
+@NgModule({
+  declarations: [
+    UpdateComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UpdateModule { }
