a8405f6f8674d663c760fc1210522716418fccc8
noelhorvath <hnoel2000@gmail.com>
2021-05-16 18:08:53 +0200

    create Period interface and add to CareTeam model

--- src/app/shared/models/careteam.model.ts
+++ src/app/shared/models/careteam.model.ts
@@ -1,10 +1,11 @@
 import {Participant} from "./participant.model";
+import {Period} from "./period.model";
 
 export interface CareTeam{
   status: string;
   category?: string[];
   name: string;
   subject?: string;
-  period?: string;
+  period?: Period;
   participant: Participant[]
 }
