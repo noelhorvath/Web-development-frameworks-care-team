646e9006b018997320cea3ea85d1a2297cea319a
noelhorvath <hnoel2000@gmail.com>
2021-05-21 23:33:42 +0200

    display email in navbar

--- src/app/pages/nav/nav.component.ts
+++ src/app/pages/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthService} from "../../services/auth.service";
+import {map, take} from "rxjs/operators";
+import firebase from "firebase";
 
 @Component({
   selector: 'app-nav',
@@ -8,10 +10,20 @@ import {AuthService} from "../../services/auth.service";
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  email: any;
 
   constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.getUserEmail();
+  }
+
+  getUserEmail(): void{
+    this.authService.getLoggedInUser().subscribe((user) =>{
+      this.email = user.email;
+    }, error => {
+      console.log(error);
+    });
   }
 
   logout() {
